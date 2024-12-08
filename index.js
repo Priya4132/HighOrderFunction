ca27374e41a74c006d39c763e283b75d28d3181b
Priya4132 <priya.aakashmishra@gmail.com>
2024-12-09 00:12:38 +0530

    code Added for Q13. of HOF

--- index.js
+++ index.js
@@ -16,3 +16,39 @@ let data=input.forEach((el,i)=>{
 
 }
 processProducts();
+
+// Q12.L0 - Filter and Sort Data with Higher-Order Functions
+let arr= [ { name: "Alice", marks: 58 },
+
+    { name: "Bob", marks: 85 },
+    
+    { name: "Charlie", marks: 92 },
+    
+    { name: "David", marks: 45 } ]
+    function processStudents(arr){
+     
+    let filteredArray=arr.filter((el,i)=>el.marks>60? el.name:null)
+    // return filteredArray
+    let sortedArray=filteredArray.sort((a,b)=>b.marks-a.marks);
+    // return sortedArray
+    let finalData=sortedArray.map((ele,i)=>ele.name)
+    return finalData
+    
+    }
+    console.log(processStudents(arr));
+
+
+    // Q13.L1 - Grouping and Summing with reduce()
+    let arr=["electronics", "clothing", "electronics", "toys", "clothing", "toys", "toys"]
+ let ans=arr.reduce((a,el,i)=>{
+   a[el]==undefined? a[el]=1:a[el]++ //if data is not defined in object assign it to value 1 or if is present increase the count
+   return a
+   },{}
+   );
+   console.log(ans);
+  // Extra Challenge
+   let ans1=Object.entries(ans); //changing object into subarray  of key -value pairs inside an array
+ console.log(ans1);
+   console.log(ans1.sort((a,b)=> a[1]<b[1]? 1:-1));//sorting result into descending order of count of categories
+   
+
